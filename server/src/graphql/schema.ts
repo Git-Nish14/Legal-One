91a35ff01febe03e96cd9cd9140f6b57c78a4428
Nish Patel <148199873+Git-Nish14@users.noreply.github.com>
2025-03-12 20:58:19 +0530

    Update schema.ts

--- server/src/graphql/schema.ts
+++ server/src/graphql/schema.ts
@@ -7,12 +7,8 @@ import { pubSub } from "./pubsub";
 
 // Function to dynamically import all resolvers
 const loadResolvers = async (): Promise<any[]> => {
-  const isProd = process.env.NODE_ENV === "production";
-  const baseDir = isProd ? "dist" : "src";
-  const fileExtension = isProd ? "js" : "ts";
-
   // Construct the resolver path pattern
-  const resolverPaths = await glob(`${baseDir}/modules/resolvers/**/*.${fileExtension}`); const resolvers = await Promise.all(
+  const resolverPaths = await glob(`dist/modules/resolvers/**/*.js`); const resolvers = await Promise.all(
     resolverPaths.map(async (resolverPath: any) => {
       const module = await import(path.resolve(resolverPath));
       return Object.values(module); // Return all exported resolvers
