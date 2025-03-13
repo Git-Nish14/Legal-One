a9fa18e434f8715084c318f7ba1483ef32edfaa8
Nish Patel <nish14062003@gmail.com>
2025-03-13 12:22:20 +0530

    Update env.ts

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
-import * as dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+// import * as dotenv from "dotenv";
+// import path from "path";
+// dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 export const ENV = {
   NODE_ENV: process.env.NODE_ENV || "development",
